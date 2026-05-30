9c982f42dfd8f074c5016ca37e235e9b8c3ad678
Pallavi Singh <anshsingh005171@gmail.com>
2026-05-30 22:46:30 +0530

    category added admin

--- src/main/java/com/example/user_management/dto/CategoryRequestDTO.java
+++ src/main/java/com/example/user_management/dto/CategoryRequestDTO.java
@@ -1,5 +1,22 @@
 package com.example.user_management.dto;
 
+
+import jakarta.validation.constraints.NotBlank;
+
 public class CategoryRequestDTO {
+
+    @NotBlank(message = "Category name is required")
+    private String name;
+
+    public CategoryRequestDTO() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
--- src/main/java/com/example/user_management/service/CategoryService.java
+++ src/main/java/com/example/user_management/service/CategoryService.java
@@ -1,5 +1,34 @@
 package com.example.user_management.service;
 
+
+import com.example.user_management.dto.CategoryRequestDTO;
+import com.example.user_management.dto.CategoryResponseDTO;
+import com.example.user_management.entity.Category;
+import com.example.user_management.repository.CategoryRepository;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class CategoryService {
 
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public CategoryResponseDTO createCategory(CategoryRequestDTO dto) {
+
+        Category category = new Category();
+        category.setName(dto.getName());
+
+        Category saved = categoryRepository.save(category);
+
+        return new CategoryResponseDTO(
+                saved.getId(),
+                saved.getName()
+        );
+    }
 }
